730621caea8b593fffbab069c4a00910bd8e9a34
Joshua Jones <joshua@general-metrics.com>
2024-08-08 01:01:09 -0700

    Update colours to Eldritch

--- user/packages/term/alacritty.nix
+++ user/packages/term/alacritty.nix
@@ -8,6 +8,37 @@
         program = "fish";
         args = [ "--login" ];
       };
+      colors = {
+        # Default colors
+        primary = {
+          background = "#212337";
+          foreground = "#ebfafa";
+        };
+
+        # Normal colors
+        normal = {
+          black = "#323449";
+          red = "#f16c75";
+          green = "#37f499";
+          yellow = "#f1fc79";
+          blue = "#7081d0";
+          magenta = "#a48cf2";
+          cyan = "#04d1f9";
+          white = "#ebfafa";
+        };
+
+        # Bright colors
+        bright = {
+          black = "#323449";
+          red = "#f16c75";
+          green = "#37f499";
+          yellow = "#f1fc79";
+          blue = "#7081d0";
+          magenta = "#a48cf2";
+          cyan = "#04d1f9";
+          white = "#ebfafa";
+        };
+      };
       font = {
         normal = {
           family = userSettings.fontFamilyTerm;
@@ -27,7 +58,6 @@
         };
         size = 16;
       };
-      # TODO: Get my old MacOS CMD+K -> clear keybinding
     };
   };
 }
--- user/packages/term/zellij.nix
+++ user/packages/term/zellij.nix
@@ -10,17 +10,17 @@
       simplified_ui = true;
       copy_command = "xclip -selection clipboard";
       theme = "custom";
-      themes.custom.fg = "#D8DEE9";
-      themes.custom.bg = "#2E3440";
-      themes.custom.black = "#3B4252";
-      themes.custom.red = "#BF616A";
-      themes.custom.green = "#A3BE8C";
-      themes.custom.yellow = "#EBCB8B";
-      themes.custom.blue = "#81A1C1";
-      themes.custom.magenta = "#B48EAD";
-      themes.custom.cyan = "#88C0D0";
-      themes.custom.white = "#E5E9F0";
-      themes.custom.orange = "#D08770";
+      themes.custom.fg = "#ebfafa";
+      themes.custom.bg = "#212337";
+      themes.custom.black = "#323449";
+      themes.custom.red = "#f16c75";
+      themes.custom.green = "#37f499";
+      themes.custom.yellow = "#f1fc79";
+      themes.custom.blue = "#a48cf2";
+      themes.custom.magenta = "#a48cf2";
+      themes.custom.cyan = "#04d1f9";
+      themes.custom.white = "#ebfafa";
+      themes.custom.orange = "#f7c67f";
     };
   };
 }
