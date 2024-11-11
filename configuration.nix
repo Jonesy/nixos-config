ecc8c924dcf13e5954c3638ee133c4c81c1b37c0
Joshua Jones <joshua@general-metrics.com>
2024-11-11 00:25:11 -0800

    Add kanata for caps lock remapping. Update packages.

--- configuration.nix
+++ configuration.nix
@@ -134,6 +134,32 @@
   environment.sessionVariables = { };
   environment.shellAliases = { };
 
+  services.kanata = {
+    enable = true;
+    keyboards = {
+      internalKeyboard = {
+        devices = [
+          "/dev/input/by-path/platform-i8042-serio-0-event-kbd"
+        ];
+        extraDefCfg = "process-unmapped-keys yes";
+        config = ''
+          (defsrc
+            caps
+          )
+          (defvar
+            tap-time 150
+            hold-time 200
+          )
+          (defalias
+            caps (tap-hold 100 100 esc lctl)
+          )
+          (deflayer base 
+            @caps
+          )
+        '';
+      };
+    };
+  };
   # Enable "experimental" flakes support
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
