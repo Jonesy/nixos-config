714dae8d3e3066af1d769320b8394f764228798c
Joshua Jones <joshua@general-metrics.com>
2025-01-09 15:55:24 -0800

    Add some settings

--- user/apps/terminal/ghostty.nix
+++ user/apps/terminal/ghostty.nix
@@ -1,3 +1,12 @@
 {pkgs, ...}: {
   home.packages = [pkgs.ghostty];
+
+  programs.ghostty = {
+    enable = true;
+    settings = {
+      background = "#212337";
+      theme = "duckbones";
+      font-family = "IosevkaTerm Nerd Font Mono";
+    };
+  };
 }
