161d658e71396934841e3d966678434b9db46bb5
Joshua Jones <joshua@general-metrics.com>
2026-06-16 23:12:13 -0700

    Tighten up wifi

--- scripts/display_wifi.sh
+++ scripts/display_wifi.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-wifi_network=$(nmcli connection show --active | awk 'NR==2 { print $1 }')
+wifi_network=$(nmcli -t -f ACTIVE,SSID dev wifi | awk -F: '$1=="yes"{print $2}')
+signal=$(nmcli -t -f ACTIVE,SIGNAL dev wifi | awk -F: '$1=="yes"{print $2}')
 icon="󰖩"
 
-if [ "$wifi_network" == "" ]; then
+if [ "$wifi_network" = "" ]; then
   icon="󱚵"
 fi
 
-printf "%s %s" "$icon" "$wifi_network"
+printf "^fg(FDF4AF)%s  ^fg()%s (%s%%)" "$icon" "$wifi_network" "$signal"
